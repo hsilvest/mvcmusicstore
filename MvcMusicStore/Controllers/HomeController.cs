1a99d04afc4110b5f20d97e4499a88a17721abd8
= <hs.ccti@gmail.com>
2014-06-14 16:51:14 -0300

    first commit

---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using MvcMusicStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var albums = AlbumRepository.GetTopSellingAlbums(5);
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcMusicStore/Repository/AlbumRepository.cs
@@ -0,0 +1,22 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore.Repository
+{
+    public class AlbumRepository
+    {
+        public static List<Album> GetTopSellingAlbums(int count)
+        {
+            MusicStoreEntities storeDB = new MusicStoreEntities();
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
